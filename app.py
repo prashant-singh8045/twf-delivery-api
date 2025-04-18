33c7f1b964405aac1412db5336778fbf77f1fe72
prashant-singh8045 <prashant.singh282000@gmail.com>
2025-04-18 21:04:46 +0530

    Updated Flask configuration for deployment

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ def home():
 
 if __name__ == '__main__':
     # Get the port number from the environment variable (or default to 5000)
-    port = os.environ.get("PORT", 5000)
+    port = int(os.environ.get("PORT", 5000))  # Ensure it's cast to an integer
     # Run the Flask app on 0.0.0.0 with the specified port
-    app.run(host="0.0.0.0", port=int(port), debug=False)
+    app.run(host="0.0.0.0", port=port, debug=False)
